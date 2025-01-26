2ee36934fba06237ec6e1662c6abd28354895ce6
택현 <tjsk2222@gmail.com>
2025-01-27 04:39:41 +0900

    커밋 테스트

---
+++ backend/src/main/java/com/springboot/member/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.springboot.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/members")
+public class MemberController {
+}
